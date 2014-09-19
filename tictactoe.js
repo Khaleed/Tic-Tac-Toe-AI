1ba41bc8222df58b182fe8064046f895b8bf2fe3
Khalid <Khalid.Ali@consultant.com>
2014-09-19 17:35:28 +0100

    refactor checkforwinner

--- tictactoe.js
+++ tictactoe.js
@@ -32,9 +32,9 @@ if (!Function.prototype.bind) { // credit to Crockford for this bind function
     };
 }
 
-var TicTacToe = function () { //current function constructor
-    this.init(); // ?? ask Eftar
-};
+var TicTacToe = function () { // current function constructor
+      this.init(); // ?? ask Eftar
+  };
 // this is the prototype obect associated with the above function constructor 
 TicTacToe.prototype = { // give all instances of TicTacToe class the following methods and values
     // every object linked to the prototype object which they inherit properties from
@@ -59,7 +59,7 @@ TicTacToe.prototype = { // give all instances of TicTacToe class the following m
         this.resetElem.onclick = function () {
             this.reset();
         }.bind(this); // this now bounded to the this parameter of the outer function 
-        // (instead of writing var that = this)
+        // (bind() instead of writing var that = this)
         this.boardElem.onclick = function (e) { // current click event handler (e is the event object passed as an arg)
             e = e || event; // event sometimes available through the global variable event (for IE)
             var source = e.boardElem || e.target; // event target is the object which
@@ -67,12 +67,12 @@ TicTacToe.prototype = { // give all instances of TicTacToe class the following m
             this.updateModel({ // parse JSON containing position of button clicked & current value
                 position: source.getAttribute("data-position"), // events bubble up the DOM Tree
                 currentValue: source.value || null // the existing value X or O in our case 
-                    //this is important so we do not allow a button to clicked twice
             })
         }.bind(this); // this now bounded to the this parameter of the outer function
     },
 
     reset: function () {
+        // each time game is reseted, re-start values 
         for (var i = 0; i <= 8; i += 1) { // null all squares 1 - 9
             this.boardArr[i] = null;
         }
@@ -83,20 +83,24 @@ TicTacToe.prototype = { // give all instances of TicTacToe class the following m
 
     // Updates the model
     updateModel: function(obj) {
-        if  (this.boardArr[obj.position] === null && this.gameOver !== true) { // check if each square is empy & game over 
+        if  (this.boardArr[obj.position] === null && this.gameOver !== true) { // check if each square is empy 
+        // & game over 
             this.moves += 1;
-            this.boardArr[obj.position] = this.xTurn ? "X" : "O"; // ternary operator: test (boolean) and exp 1 if truthy
-            // & exp2 if falsy  
-            this.xTurn = !this.xTurn; // take turns between X and 0
+            this.boardArr[obj.position] = this.xTurn ? "X" : "O"; // ternary operator: test (boolean) and 
+            // exp 1 if truthy & exp2 if falsy  
+            this.xTurn = !this.xTurn; // enable taking turns between X and O
             this.statusElem.innerHTML = "It is the turn of " + (this.xTurn ? "X" : "O");
         }
-        if  (this.boardArr === "")
-            this.checkForWinner(); // update checkForWinner() using the boardArr
+        if  (this.boardArr === " ")
+            this.checkForWinner(); // update checkForWinner method using the board array
     },
 
     checkForWinner: function () { // check all 8 winning combinations in boardArr
-        
-        // check if there are three Xs or three Os in a row  
+        // check if there are three Xs or three Os in a row
+        var winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], 
+                            [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+                            [0, 4, 8], [2, 4, 6]
+        ];  
         if (arr[1] === arr[2] && arr[2] === arr[3] && arr[1] !== "") {
             this.resultElem.innerHTML = arr[1] + ' wins';
             this.gameOver = true;
