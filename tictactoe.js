cb6183498b82914cc16931feb8a57e8499927eff
Khalid <Khalid.Ali@consultant.com>
2014-10-23 18:42:16 -0400

    update UI ver 1

--- tictactoe.js
+++ tictactoe.js
@@ -1,15 +1,3 @@
-if (!String.prototype.supplant) { // credit to Crockford for this supplant function
-    String.prototype.supplant = function (o) {
-        return this.replace(
-            /\{([^{}]*)\}/g,
-            function (a, b) {
-                var r = o[b];
-                return typeof r === 'string' || typeof r === 'number' ? r : a;
-            }
-        );
-    };
-}
-
 if (!Function.prototype.bind) { // credit to Crockford for this bind function  
     Function.prototype.bind = function (oThis) {
         if (typeof this !== "function") {
@@ -28,12 +16,12 @@ if (!Function.prototype.bind) { // credit to Crockford for this bind function
         fNOP.prototype = this.prototype;
         fBound.prototype = new fNOP();
 
-        return fBound;
+        return fBound; // NYC 
     };
 }
 
-var TicTacToe = function () { // current function constructor 
-    this.init(); 
+var TicTacToe = function() { // current function constructor 
+    this.init();
 };
 // this is the prototype obect associated with the above function constructor 
 TicTacToe.prototype = { // give all instances of TicTacToe class the following methods and values
@@ -47,11 +35,11 @@ TicTacToe.prototype = { // give all instances of TicTacToe class the following m
     gameOver: false,
     moves: 0,
     boardArr: [],
-    winningCombo: [
+    winCombo: [
 
         [0, 1, 2],
         [3, 4, 5],
-        [6, 7, 8], 
+        [6, 7, 8],
         [0, 3, 6],
         [1, 4, 7],
         [2, 5, 8],
@@ -60,28 +48,29 @@ TicTacToe.prototype = { // give all instances of TicTacToe class the following m
 
     ],
 
-    init: function () { // first function associated with the prototype 
+    init: function() { // first function associated with the prototype 
         // bind UI
-        this.resetElem = document.getElementById("reset-button");
+        this.resetElem = document.getElementById("reset");
         this.boardElem = document.getElementById("game-board");
         this.resultElem = document.getElementById("results");
         this.statusElem = document.getElementById("status");
-        this.reset();
-        // current binded events on clicking reset and gameboard
-        this.resetElem.onclick = function () {
-            this.reset();
+        this.resetGame();
+        // current binded events on clicking resetGame and gameboard
+        this.resetElem.onclick = function() {
+            this.resetGame();
         }.bind(this); // this now bounded to the this parameter of the outer function 
-        // (bind() instead of writing var that = this)
-        this.boardElem.onclick = function (e) { // current click event handler (e is the event object passed as an arg)
+        // (bind() instead of writing var that = this
+        this.boardElem.onclick = function(e) { // current click event handler (e is the event object passed as an arg)
             e = e || event; // event sometimes available through the global variable event (for IE)
             var source = e.boardElem || e.target; // event target is the object which
             // the event is associated
-            this.updateModel(source.getAttribute("data-position")); // events bubble up the DOM Tree
+            this.updateGame(source.getAttribute("data-position")); // events bubble up the DOM Tree
+            this.updateUI(source.getAttribute("data-position"));
         }.bind(this); // this now bounded to the this parameter of the outer function
     },
 
-    reset: function () {
-        for (var i = 0; i <= 8; i += 1) { 
+    resetGame: function() {
+        for (var i = 0; i <= 8; i += 1) {
             this.boardArr[i] = null; // initialise all square values with null
         }
         this.moves = 0;
@@ -89,35 +78,53 @@ TicTacToe.prototype = { // give all instances of TicTacToe class the following m
         this.xTurn = true; // X is first player 
     },
 
-    checkForWinningMove: function () { // check all 8 winning combinations
+    checkForWinningMove: function() { // check all 8 winning combinations
         // check 3 rows, 3 columns and 2 diagonals
-        for (var i = 0; i < this.winningCombo.length; i += 1) {
-            if (this.boardArr[this.winningCombo[i][0]] === this.boardArr[this.winningCombo[i][1]] && this.boardArr[this.winningCombo[i][1]] 
-                === this.boardArr[this.winningCombo[i][2]] && this.boardArr[this.winningCombo[i][0]] !== null) {
+        for (var i = 0; i < this.winCombo.length; i += 1) {
+            if (this.boardArr[this.winCombo[i][0]] === this.boardArr[this.winCombo[i][1]] && this.boardArr[this.winCombo[i][1]] 
+                === this.boardArr[this.winCombo[i][2]] && this.boardArr[this.winCombo[i][0]] !== null) {
                 this.gameOver = true;
                 return true;
             }
         }
     },
 
+    updateUI: function(position) {
+        var i, square = document.querySelectorAll("input[type='button']"); // returns an array of all buttons clicked
+        for (i = 0; i < square.length; i += 1) {
+            square[i].value = this.boardArr[position];
+        }
+    }, 
+    
     // Updates the model
-    updateModel: function (position) {
+    updateGame: function(position) {
+        var square;
         if (this.boardArr[position] === null && this.gameOver !== true) { // check board is empty and game is not over
             this.moves += 1; // increment game moves
-            this.boardArr[position] = this.xTurn ? "X" : "O"; // current move
-            this.xTurn = !this.xTurn; // current game turn 
+            this.boardArr[position] = this.xTurn ? "X" : "O"; // current move 
+            this.xTurn = !this.xTurn; // current game turn
+            this.updateUI();
             this.statusElem.innerHTML = "It is the turn of " + (this.xTurn ? "X" : "O"); // current game status
         }
         var outcome = this.checkForWinningMove(); // check if there is a winning move
         if (outcome === true) { // decide who won
-            if (this.xTurn === false) { // when X wins, this.Xturn will be false 
-                this.reset(); // clear the board
+            if (this.xTurn === false) { // when X wins, this.Xturn will be false
+                alert("X wins");
+                this.resetGame(); 
             } else {
                 alert("O wins");
-                this.rest(); // clear the board
+                this.resetGame();
             }
         }
     }
 };
 
-var playGame = new TicTacToe(); 
+var playGame = new TicTacToe();
+// Update UI (do it at home) 
+// AI - naive player (randomly)n
+// AI
+// naive computer player
+// two in a row - fork try & win
+// encode strategies from wiki 
+// advanced AI 
+
