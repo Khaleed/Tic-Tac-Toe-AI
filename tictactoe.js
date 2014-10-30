e2391794f320e25d26b783d82ff397d5debbb39b
Khalid <Khalid.Ali@consultant.com>
2014-10-30 18:42:04 -0400

    testing 1

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,5 @@
 if (!Function.prototype.bind) { // credit to Crockford for this bind function  
-    Function.prototype.bind = function (oThis) {
+    Function.prototype.bind = function(oThis) {
         if (typeof this !== "function") {
             // closest thing possible to the ECMAScript 5 internal IsCallable function
             throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
@@ -7,8 +7,8 @@ if (!Function.prototype.bind) { // credit to Crockford for this bind function
 
         var aArgs = Array.prototype.slice.call(arguments, 1),
             fToBind = this,
-            fNOP = function () {},
-            fBound = function () {
+            fNOP = function() {},
+            fBound = function() {
                 return fToBind.apply(this instanceof fNOP && oThis ? this : oThis,
                     aArgs.concat(Array.prototype.slice.call(arguments)));
             };
@@ -16,14 +16,14 @@ if (!Function.prototype.bind) { // credit to Crockford for this bind function
         fNOP.prototype = this.prototype;
         fBound.prototype = new fNOP();
 
-        return fBound; // NYC 
+        return fBound;
     };
 }
 
 var TicTacToe = function() { // current function constructor 
     this.init();
 };
-// this is the prototype obect associated with the above function constructor 
+// this is the prototype object associated with the above function constructor 
 TicTacToe.prototype = { // give all instances of TicTacToe class the following methods and values
     // every object linked to the prototype object which they inherit properties from
     // the prototype object is where inherited methods and values are deposited
@@ -62,15 +62,17 @@ TicTacToe.prototype = { // give all instances of TicTacToe class the following m
         // (bind() instead of writing var that = this
         this.boardElem.onclick = function(e) { // current click event handler (e is the event object passed as an arg)
             e = e || event; // event sometimes available through the global variable event (for IE)
-            var source = e.boardElem || e.target; // event target is the object which
-            // the event is associated
-            this.updateGame(source.getAttribute("data-position")); // events bubble up the DOM Tree
-            this.updateUI(source.getAttribute("data-position"), source);
+            var source = e.boardElem || e.target; // event target is the object which the event is associated
+            this.updateGame({ // when boardArr is clicked, pass an object containing the element and its position
+                position: source.getAttribute("data-position"),
+                element: source
+            });
         }.bind(this); // this now bounded to the this parameter of the outer function
     },
 
     resetGame: function() {
-        for (var i = 0; i <= 8; i += 1) {
+        var i;
+        for (i = 0; i <= 8; i += 1) {
             this.boardArr[i] = null; // initialise all square values with null
         }
         this.moves = 0;
@@ -80,29 +82,26 @@ TicTacToe.prototype = { // give all instances of TicTacToe class the following m
 
     checkForWinningMove: function() { // check all 8 winning combinations
         // check 3 rows, 3 columns and 2 diagonals
-        for (var i = 0; i < this.winCombo.length; i += 1) {
+        var i;
+        for (i = 0; i < this.winCombo.length; i += 1) {
             if (this.boardArr[this.winCombo[i][0]] === this.boardArr[this.winCombo[i][1]] && this.boardArr[this.winCombo[i][1]] 
                 === this.boardArr[this.winCombo[i][2]] && this.boardArr[this.winCombo[i][0]] !== null) {
                 this.gameOver = true;
                 return true;
             }
+            return false;  
         }
     },
 
-    updateUI: function(position, element) {
-        var square = input.value;
-        square = this.boardArr[position];
-    }, 
-    
-    // Updates the model
-    updateGame: function(position) {
+    updateGame: function(object) {
         var square;
-        if (this.boardArr[position] === null && this.gameOver !== true) { // check board is empty and game is not over
+        if (this.boardArr[object.position] === null && this.gameOver !== true) { // check board array is empty and game isn't over
             this.moves += 1; // increment game moves
-            this.boardArr[position] = this.xTurn ? "X" : "O"; // current move 
+            this.boardArr[object.position] = this.xTurn ? "X" : "O"; // current move 
             this.xTurn = !this.xTurn; // current game turn
-            this.updateUI();
-            this.statusElem.innerHTML = "It is the turn of " + (this.xTurn ? "X" : "O"); // current game status
+            square = object.element;
+            square.value = this.boardArr[object.position]; 
+            this.statusElem.innerHTML = "It's the turn of " + (this.xTurn ? "X" : "O"); // current game status
         }
         var outcome = this.checkForWinningMove(); // check if there is a winning move
         if (outcome === true) { // decide who won
@@ -112,15 +111,11 @@ TicTacToe.prototype = { // give all instances of TicTacToe class the following m
             } else {
                 alert("O wins");
                 this.resetGame();
+                return;
             }
         }
     }
+
 };
 
 var playGame = new TicTacToe();
-// AI - naive player (randomly)n
-// naive computer player
-// two in a row - fork try & win
-// encode strategies from wiki 
-// advanced AI 
-
